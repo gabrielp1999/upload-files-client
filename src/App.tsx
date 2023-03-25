ffe862513882277ad5cbe33035413f59825eb8c3
Gabriel Pereira <gabrielpereira@MacBook-Air-de-Gabriel.local>
2023-03-25 15:19:24 -0300

    feat: toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import uploadFile from './service/uploadFile'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [file, setFile] = useState<File | null>(null);
+  const [linkFile, setLinkFile] = useState("");
 
   const fileChange = (e: any) => {
     setFile(e.target.files[0]);
@@ -14,14 +17,29 @@ function App() {
       alert("Não existe arquivo");
       return;
     }
-    uploadFile(file);
+    toast.success("Upload concluido", {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+    uploadFile(file)
+    
+    .then((res) => {
+      if(res.status === 200){
+        setLinkFile(res.url)
+      }
+    })
   }
-
 
   return (
     <div className='app'>
       <div className="file-upload-container">
-        <h1>Upload de Arquivos</h1>
+        <h2 className='name-file'>Upload de Arquivos</h2>
         <input 
           type="file" 
           onChange={fileChange}
@@ -38,9 +56,28 @@ function App() {
 
         <button
           onClick={() => sendFile()}
+          className="btn"
         >
           Enviar Arquivo
         </button>
+
+          {linkFile && 
+          <div>
+            <h5 className='name-file'>Link do arquivo salvo no servidor:</h5>
+            <a target="_blank" href={linkFile}>{linkFile}</a>
+          </div>}
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </div>
     </div>
   )
